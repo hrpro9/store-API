b6f4205025f7b34f5a44b71f1788779652671b79
abdelhalim <abdelhalimpro@gmail.com>
2021-10-23 16:31:02 +0000

    initial commit

--- storefront/settings.py
+++ storefront/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': '192.168.2.40',
+        'USER': 'storeAdmin',
+        'PASSWORD': 'Test@123',
+        
     }
 }
 
