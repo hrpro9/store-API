8c0c3fa69f1e4543782d6e4d3bf72798cc6b05fb
abdelhalim <abdelhalimpro@gmail.com>
2021-10-23 17:11:24 +0000

    start ORM

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 
 def say_hello(request):
-    x=1
-    y=5
+    Product.objects.all()
+
     return render(request, 'hello.html', { 'name':'abdelhalim'})
