f0a1a1f7ff2b1f2e11cd23e1d9d81525f94e5cae
abdelhalim <abdelhalimpro@gmail.com>
2021-10-25 14:30:50 +0000

    finish without site admin

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,40 @@
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q, F
+from django.db import transaction
 from store.models import Product
+from tags.models import TaggedItem
+
+# @transaction.atomic()
 
 
 def say_hello(request):
-    Product.objects.all()
+    # # AND
+    # queryset = Product.objects.filter(unit_price__range=(20, 30), collection__gt=900)
 
-    return render(request, 'hello.html', { 'name':'abdelhalim'})
+    # # OR
+    # queryset = Product.objects.filter(
+    #     Q(unit_price__range=(20, 30)) | Q(collection__gt=900))
+
+    # # Compare fields
+    # queryset = Product.objects.filter(collection=F('id'))
+
+    # # inverse sorting
+    # queryset = Product.objects.order_by('-unit_price')
+
+    # # get only some culomns
+    # queryset = Product.objects.values_list('id', 'title', 'collection__title')
+
+    # # related tables
+    # queryset = Product.objects.select_related('collection').all()
+
+    # do transaction
+    with transaction.atomic():
+        # Related apps
+        queryset = TaggedItem.unpublished_objects.get_tags_for(Product, 1)
+
+    # row sql querys
+    queryset = Product.objects.raw('SELECT * FROM store_product')
+
+    return render(request, 'hello.html', {'name': 'abdelhalim', 'products': list(queryset)})
--- storefront/settings.py
+++ storefront/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'django.contrib.sessions',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.messages',
@@ -91,7 +92,7 @@ DATABASES = {
         'HOST': '192.168.2.40',
         'USER': 'storeAdmin',
         'PASSWORD': 'Test@123',
-        
+
     }
 }
 
