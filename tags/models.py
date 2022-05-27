8f5fd455effa8d2c71a7077b0fa5539bbe9c9e8a
abhelhalim hriche <hriche.abdelhalim@gmail.com>
2022-05-27 23:22:48 +0100

    update with newer version

--- tags/models.py
+++ tags/models.py
@@ -1,27 +1,30 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-from django.db.models import query
 
 
-class TaggedItemManager(models.Model):
+class TaggedItemManager(models.Manager):
     def get_tags_for(self, obj_type, obj_id):
-        contentType = ContentType.objects.get_for_model(obj_type)
-        return TaggedItem.objects\
-            .select_related('tag')\
-            .filter(content_type=contentType, object_id=obj_id)
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        return TaggedItem.objects \
+            .select_related('tag') \
+            .filter(
+                content_type=content_type,
+                object_id=obj_id
+            )
 
 
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.label
+
 
 class TaggedItem(models.Model):
+    objects = TaggedItemManager()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    # Type (product, video, article)
-    # ID
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
-
-    unpublished_objects = TaggedItemManager()
